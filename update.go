c090a13bf4a418e6442de20c117875ad757c0bd7
Lukin <mylukin@gmail.com>
2020-04-07 01:16:42 +0800

    add update.go

---
+++ update.go
@@ -0,0 +1,93 @@
+package easytgbot
+
+import (
+	"fmt"
+)
+
+// Update is message
+type Update struct {
+	JSON
+}
+
+// Chat get update
+func (update *Update) Chat() (JSON, error) {
+	message := update.Get("message")
+	if message.Exists() {
+		return message.Get("chat"), nil
+	}
+
+	editedMessage := update.Get("edited_message")
+	if editedMessage.Exists() {
+		return editedMessage.Get("chat"), nil
+	}
+
+	channelPost := update.Get("channel_post")
+	if channelPost.Exists() {
+		return channelPost.Get("chat"), nil
+	}
+
+	editedChannelPost := update.Get("edited_channel_post")
+	if editedChannelPost.Exists() {
+		return editedChannelPost.Get("chat"), nil
+	}
+
+	callbackQuery := update.Get("callback_query")
+	if callbackQuery.Exists() {
+		message := callbackQuery.Get("message")
+		if message.Exists() {
+			return message.Get("chat"), nil
+		}
+	}
+
+	return JSON{}, fmt.Errorf("chat is not found")
+}
+
+// From get update
+func (update *Update) From() (JSON, error) {
+	message := update.Get("message")
+	if message.Exists() {
+		return message.Get("from"), nil
+	}
+
+	editedMessage := update.Get("edited_message")
+	if editedMessage.Exists() {
+		return editedMessage.Get("from"), nil
+	}
+
+	channelPost := update.Get("channel_post")
+	if channelPost.Exists() {
+		return channelPost.Get("from"), nil
+	}
+
+	editedChannelPost := update.Get("edited_channel_post")
+	if editedChannelPost.Exists() {
+		return editedChannelPost.Get("from"), nil
+	}
+
+	callbackQuery := update.Get("callback_query")
+	if callbackQuery.Exists() {
+		return callbackQuery.Get("from"), nil
+	}
+
+	inlineQuery := update.Get("inline_query")
+	if inlineQuery.Exists() {
+		return inlineQuery.Get("from"), nil
+	}
+
+	shippingQuery := update.Get("shipping_query")
+	if shippingQuery.Exists() {
+		return shippingQuery.Get("from"), nil
+	}
+
+	preCheckoutQuery := update.Get("pre_checkout_query")
+	if preCheckoutQuery.Exists() {
+		return preCheckoutQuery.Get("from"), nil
+	}
+
+	chosenInlineResult := update.Get("chosen_inline_result")
+	if chosenInlineResult.Exists() {
+		return chosenInlineResult.Get("from"), nil
+	}
+
+	return JSON{}, fmt.Errorf("from is not found")
+}
